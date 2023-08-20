0f6341a8a10840c272e7dd9c14191e21d69d284f
Adewunmi Olaniyi Oluwaseyi <108165968+protechanalysis@users.noreply.github.com>
2023-08-20 17:47:06 +0100

    Update Bonus Question.sql

--- Bonus Question.sql
+++ Bonus Question.sql
@@ -2,23 +2,23 @@
 ----- Write an INSERT statement to demonstrate what would happen if a new
 ----- 'Supreme' pizza with all the toppings was added to the Pizza Runner menu?
 
------ creating a new table and update of 'pizza_recipes' adding another pizza to it.  
+----- updating 'pizza_recipes' by adding another pizza to it.  
 
 INSERT INTO pizza_recipes
   (pizza_id, toppings)
 VALUES
   (3, '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12');
   
------ check it the new the table
+----- check if the update was effective
  select *
  from pizza_recipes_update;
   
------ creating a new table and update of 'pizza_names' adding another pizza to it.
+----- updating 'pizza_names' by adding another pizza to it.
 INSERT INTO pizza_names
   (pizza_id, pizza_name)
 VALUES
   (3, 'Supreme');
 
------ checking it the new the table
+----- check if the update was effective
 select *
 from pizza_names
